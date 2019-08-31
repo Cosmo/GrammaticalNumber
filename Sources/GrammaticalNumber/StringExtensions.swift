c40b3289103fc53627e5c2b76204c4a5d5ff3fc9
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-08-31 10:33:32 +0200

    Add pluralized() and singularized()

---
+++ Sources/GrammaticalNumber/StringExtensions.swift
@@ -0,0 +1,65 @@
+import Foundation
+
+extension String {
+    private func prependCount(_ count: Int?) -> String {
+        guard let count = count else { return self }
+        return [String(count), self].joined(separator: " ")
+    }
+    
+    func pluralized(count: Int? = nil, language: String = GrammaticalNumberRule.defaultLanguage) -> String {
+        var word = self
+        
+        if count == 1 {
+            return word.prependCount(count)
+        }
+        
+        guard let rules = GrammaticalNumberRule.rules[language] else {
+            return word.prependCount(count)
+        }
+        
+        guard let rule = (rules.reversed().first { grammaticalNumberRule -> Bool in
+            switch grammaticalNumberRule {
+            case .uncountable(let rule), .irregular(let rule, _):
+                return self.contains(rule)
+            case .plural(let rule, _):
+                return self.range(of: rule, options: .regularExpression, range: nil, locale: nil) != nil
+            default: return false
+            }
+        }) else {
+            return word.prependCount(count)
+        }
+        
+        switch rule {
+        case .irregular(let rule, let replacement), .plural(let rule, let replacement):
+            word = word.replacingOccurrences(of: rule, with: replacement, options: String.CompareOptions.regularExpression)
+        default: break
+        }
+        
+        return word.prependCount(count)
+    }
+    
+    func singularized(language: String = GrammaticalNumberRule.defaultLanguage) -> String {
+        guard let rules = GrammaticalNumberRule.rules[language] else {
+            return self
+        }
+        
+        guard let rule = (rules.reversed().first { grammaticalNumberRule -> Bool in
+            switch grammaticalNumberRule {
+            case .uncountable(let rule), .irregular(_, let rule):
+                return self.contains(rule)
+            case .singular(let rule, _):
+                return self.range(of: rule, options: .regularExpression, range: nil, locale: nil) != nil
+            default: return false
+            }
+        }) else {
+            return self
+        }
+        
+        switch rule {
+        case .irregular(let replacement, let rule), .singular(let rule, let replacement):
+            return self.replacingOccurrences(of: rule, with: replacement, options: String.CompareOptions.regularExpression)
+        default:
+            return self
+        }
+    }
+}
